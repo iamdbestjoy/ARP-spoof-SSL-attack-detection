255c5494651b6234566ff0e67bb64d555fdb2849
iamdbestjoy <iamdbestjoy@gmail.com>
2015-04-15 12:14:15 -0700

    Update cronscript.sh

--- cronscript.sh
+++ cronscript.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+arp -an | awk '{print $4}' | sort | uniq -c | grep -v ' 1 '
+ 
+if [ "$?" -eq 0 ]
+then
+        /usr/bin/zenity  --warning  --text="Arp Spoofing is detected. You will be disconnected from the network for your safety" --title="Phishing Warning"
+sudo ifconfig lo down
+fi
